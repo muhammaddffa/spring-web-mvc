e418e698faf3ba730b776e450150d33898845c80
muhammaddffa <muhammaddaffa463@gmail.com>
2025-01-22 01:22:48 +0700

    Response Content Type

--- src/main/java/learnspringboot/webmvc/controller/FormController.java
+++ src/main/java/learnspringboot/webmvc/controller/FormController.java
@@ -10,10 +10,22 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class FormController {
 
-    @PostMapping(path = "/form/hello", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    @PostMapping(
+            path = "/form/hello",
+            //Untuk Menerima data jenis apa
+            consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE,
+            //Untuk Membuat data untuk membuat apa
+            produces = MediaType.TEXT_HTML_VALUE
+    )
     @ResponseBody
     public String hello(@RequestParam(name = "name") String name){
-        return "Hello " + name;
+        return """
+                <html>
+                <body>
+                <h1>Hello $name</h1>
+                <body>
+                <html>
+                """.replace("$name", name);
     }
 
 }
--- src/test/java/learnspringboot/webmvc/controller/FormControllerTest.java
+++ src/test/java/learnspringboot/webmvc/controller/FormControllerTest.java
@@ -5,12 +5,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -27,6 +28,7 @@ public class FormControllerTest {
                         .param("name", "Dade")
         ).andExpectAll(
                 status().isOk(),
+                header().string(HttpHeaders.CONTENT_TYPE, Matchers.containsString(MediaType.TEXT_HTML_VALUE)),
                 content().string(Matchers.containsString("Hello Dade"))
         );
     }
